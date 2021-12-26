55576c3eb8bf148c0388ad3f223ce75af8571460
tk1351 <takakikitsui0111@gmail.com>
2021-12-27 08:32:07 +0900

    chore: vuex, test環境構築

---
+++ app/test/e2e/custom-commands/trigger.js
@@ -0,0 +1,10 @@
+exports.command = function (selector, event, keyCode) {
+  return this.execute(function (selector, event, keyCode) {
+    const e = document.createEvent('HTMLEvents')
+    e.initEvent(event, true, true)
+    if (keyCode) {
+      e.keyCode = keyCode
+    }
+    document.querySelector(selector).dispatchEvent(e)
+  }, [selector, event, keyCode])
+}
