68c3e454e2eb2414eed79a6cad4020c898a382f1
tk1351 <takakikitsui0111@gmail.com>
2021-12-27 09:15:47 +0900

    chore: 設計まで

---
+++ app/src/router/routes.js
@@ -0,0 +1,24 @@
+import KbnBoardView from '../components/templates/KbnBoardView'
+import KbnLoginView from '../components/templates/KbnLoginView'
+import KbnTaskDetailModal from '../components/templates/KbnTaskDetailModal'
+
+export default [
+  {
+    path: '/',
+    component: KbnBoardView,
+    meta: {requiresAuth: true}
+  },
+  {
+    path: '/login',
+    component: KbnLoginView,
+  },
+  {
+    path: '/tasks/:id',
+    component: KbnTaskDetailModal,
+    meta: {requiresAuth: true}
+  },
+  {
+    path: '*',
+    redirect: '/'
+  }
+]
--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import getters from './getters'
+import actions from './actions'
+import mutations from "./mutations";
 
 Vue.use(Vuex)
 
+const state = {
+  auth: {
+    token: null,
+    userId: null
+  },
+  board: {
+    lists: []
+  }
+}
+
 export default new Vuex.Store({
-  // getters,
-  // actions,
-  // mutations,
+  state,
+  getters,
+  actions,
+  mutations,
   strict: process.env.NODE_ENV !== 'production'
 })
